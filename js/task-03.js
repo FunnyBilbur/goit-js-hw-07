19db1f0d9f0b2599e4bca543e8714cfbb1d5e4dc
FunnyBilbur <tonistark1997@gmail.com>
2023-12-01 16:34:04 +0300

    task-03 and task-04

--- js/task-03.js
+++ js/task-03.js
@@ -4,7 +4,7 @@ const NameOutput = document.querySelector('#name-output');
 NameInput.addEventListener('input', (event) => {
     // console.log('Changed');
     NameOutput.textContent = event.currentTarget.value;
-    if (event.currentTarget.value.replace(/\s+/g, '').trim() == '') {
+    if (event.currentTarget.value.trim() == '') {
         NameOutput.textContent = 'Anonymous';
     }
     // console.dir(NameInput);
--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,18 @@
+const login_form = document.querySelector('.login-form');
+// console.dir(login_form);
+login_form.addEventListener('submit', onSubmitFunct);
 
+function onSubmitFunct(params) {
+    params.preventDefault();//сбрасываем стандартные свойства для сабмита формы, которая перебрасывает на отдельное окно
+    const { email, password } = params.currentTarget.elements;
+    if (email.value === '' || password.value === '') {
+        alert('All form fields must be filled in');
+    } else {
+        const data = {
+            email: email.value.trim(),
+            password: password.value.trim(),
+        }
+        console.log(data);
+    }
+    params.currentTarget.reset();
+}
