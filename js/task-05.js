d0828bcb43bb6b88b92be7a0d5683855c5e73e34
FunnyBilbur <tonistark1997@gmail.com>
2023-12-01 16:37:46 +0300

    task-05

--- js/task-05.js
+++ js/task-05.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const change_color = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+const body = document.querySelector('body');
+console.dir(color);
+change_color.addEventListener('click', onClick);
+
+function onClick() {
+  const randomColor = getRandomHexColor();
+  body.setAttribute('style', `background-color:${randomColor}`);
+  color.textContent = randomColor;
+}
